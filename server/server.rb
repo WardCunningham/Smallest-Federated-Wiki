92903b9746a77d8c40589399a62ed4ce0909bc66
Ward Cunningham <ward@c2.com>
2011-06-26 16:23:12 -0700

    internal links

--- server/server.rb
+++ server/server.rb
@@ -7,7 +7,16 @@ require 'json'
 helpers do  
   def get_page name
     File.open("data/pages/#{name}", 'r') { |file| JSON.parse(file.read)}
-  end 
+  end
+  def resolve_links string
+    string.gsub(/\[\[([a-z-]+)\]\]/, '<a href="/page/\1">\1</a>')
+  end
+  def render_page name
+    page = get_page name
+    title = page['title']
+    paragraphs = page['body'].collect {|each| "<p>#{each['text']}</p>"}
+    "<h1>#{$identity['title']}</h1><h2>#{title}</h2>#{resolve_links(paragraphs.join("\n"))}"
+  end
 end
 
 configure do
@@ -15,15 +24,9 @@ configure do
 end
 
 get '/' do
-  page = get_page($identity['root'])
-  title = page['title']
-  paragraphs = page['body'].collect {|each| "<p>#{each['text']}</p>"}
-  "<h1>#{$identity['title']}</h1><h2>#{title}</h2>#{paragraphs.join("\n")}</pre>"
+  render_page($identity['root'])
 end
 
 get '/page/:name' do |name|
-  page = get_page(name)
-  title = page['title']
-  paragraphs = page['body'].collect {|each| "<p>#{each['text']}</p>"}
-  "<h1>#{$identity['title']}</h1><h2>#{title}</h2>#{paragraphs.join("\n")}</pre>"
+  render_page(name)
 end
