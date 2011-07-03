2e54893bbfb17fcec1563be21dee22f1569931c1
Ward Cunningham <ward@c2.com>
2011-07-02 23:28:54 -0700

    journal might be absent

--- server/server.rb
+++ server/server.rb
@@ -47,7 +47,7 @@ put %r{^/page/([a-z-]+)/edit$} do |name|
   page = get_page(name)
   edit = JSON.parse(params['edit'])
   page['body'] = edit['order'].collect{ |id| page['body'].detect{ |item| item['id'] == id } }
-  page['journal'] << edit
+  ( page['journal'] ||= [] ) << edit # todo: journal undo, not redo
   put_page name, page
   "ok"
 end
