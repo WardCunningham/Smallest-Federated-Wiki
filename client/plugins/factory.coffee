0aa83490bd5466f1d82b26e94ab66910d6d29b9b
Ward Cunningham <ward@c2.com>
2011-10-16 16:51:04 -0700

    simplify drop logic

--- client/plugins/factory.coffee
+++ client/plugins/factory.coffee
@@ -1,45 +1,44 @@
 window.plugins.factory =
   emit: (div, item) -> div.append '<p>Double-Click to Edit<br>Drop Text or Image to Insert</p>'
   bind: (div, item) ->
-    bindDragAndDrop(div, item, ["image", "text"])
     div.dblclick ->
       div.removeClass('factory').addClass(item.type='paragraph')
       textEditor div, item
+    div.bind 'dragenter', (evt) -> evt.preventDefault()
+    div.bind 'dragover', (evt) -> evt.preventDefault()
+    div.bind "drop", (dropEvent) ->
 
-bindDragAndDrop = (div, item, allowedTypes = []) ->
-  ["dragenter", "dragover"].map (eventName) ->
-    div.bind eventName, (evt) -> evt.preventDefault()
+      finishDrop = (type, handler) ->
+        (loadEvent) ->
+          item.type = type
+          handler loadEvent
+          div.empty()
+          div.removeClass("factory").addClass(type)
+          pageDiv = div.parents('.page:first')
+          action = {type: 'edit', id: item.id, item: item}
+          wiki.putAction(pageDiv, action)
+          wiki.getPlugin(type).emit(div, item)
 
-  div.bind "drop", (dropEvent) ->
-
-    finishDrop = (type, handler) ->
-      (loadEvent) ->
-        item.type = type
-        handler loadEvent
-        div.empty()
-        div.removeClass("factory").addClass(type)
-        pageDiv = div.parents('.page:first')
-        action = {type: 'edit', id: item.id, item: item}
-        wiki.putAction(pageDiv, action)
-        wiki.getPlugin(type).emit(div, item)
-
-    dropEvent.preventDefault()
-    console.log [dropEvent, dropEvent.originalEvent]
-    file = dropEvent.originalEvent.dataTransfer.files[0]
-    console.log [file.type, file]
-    [majorType, minorType] = file.type.split("/")
-
-    if allowedTypes.filter((t) -> t == majorType).length == 0
-      alert("Uploads of type #{majorType} not supported for this item")
-    else
-      reader = new FileReader()
-      if majorType == "image"
-        reader.onload = finishDrop "image", (loadEvent) ->
-          item.url = loadEvent.target.result
-          item.caption ||= "Uploaded image"
-        reader.readAsDataURL(file)
-      else if majorType == "text"
-        reader.onload = finishDrop "paragraph", (loadEvent) ->
-          item.text = loadEvent.target.result
-        reader.readAsText(file)
+      dropEvent.preventDefault()
+      if dropEvent.originalEvent.dataTransfer?
+        console.log dropEvent.originalEvent.dataTransfer.types
+        file = dropEvent.originalEvent.dataTransfer.files[0]
+        if file?
+          [majorType, minorType] = file.type.split("/")
+          reader = new FileReader()
+          if majorType == "image"
+            reader.onload = finishDrop "image", (loadEvent) ->
+              item.url = loadEvent.target.result
+              item.caption ||= "Uploaded image"
+            reader.readAsDataURL(file)
+          else if majorType == "text"
+            reader.onload = finishDrop "paragraph", (loadEvent) ->
+              item.text = loadEvent.target.result
+            reader.readAsText(file)
+          else
+            alert "Expected text or image, got '#{file.type}'"
+        else
+          alert "Expected file, got null"
+      else
+        alert "Expected dataTransfer, got null"
 
--- client/plugins/factory.js
+++ client/plugins/factory.js
@@ -1,69 +1,66 @@
 (function() {
-  var bindDragAndDrop;
   window.plugins.factory = {
     emit: function(div, item) {
       return div.append('<p>Double-Click to Edit<br>Drop Text or Image to Insert</p>');
     },
     bind: function(div, item) {
-      bindDragAndDrop(div, item, ["image", "text"]);
-      return div.dblclick(function() {
+      div.dblclick(function() {
         div.removeClass('factory').addClass(item.type = 'paragraph');
         return textEditor(div, item);
       });
-    }
-  };
-  bindDragAndDrop = function(div, item, allowedTypes) {
-    if (allowedTypes == null) {
-      allowedTypes = [];
-    }
-    ["dragenter", "dragover"].map(function(eventName) {
-      return div.bind(eventName, function(evt) {
+      div.bind('dragenter', function(evt) {
         return evt.preventDefault();
       });
-    });
-    return div.bind("drop", function(dropEvent) {
-      var file, finishDrop, majorType, minorType, reader, _ref;
-      finishDrop = function(type, handler) {
-        return function(loadEvent) {
-          var action, pageDiv;
-          item.type = type;
-          handler(loadEvent);
-          div.empty();
-          div.removeClass("factory").addClass(type);
-          pageDiv = div.parents('.page:first');
-          action = {
-            type: 'edit',
-            id: item.id,
-            item: item
+      div.bind('dragover', function(evt) {
+        return evt.preventDefault();
+      });
+      return div.bind("drop", function(dropEvent) {
+        var file, finishDrop, majorType, minorType, reader, _ref;
+        finishDrop = function(type, handler) {
+          return function(loadEvent) {
+            var action, pageDiv;
+            item.type = type;
+            handler(loadEvent);
+            div.empty();
+            div.removeClass("factory").addClass(type);
+            pageDiv = div.parents('.page:first');
+            action = {
+              type: 'edit',
+              id: item.id,
+              item: item
+            };
+            wiki.putAction(pageDiv, action);
+            return wiki.getPlugin(type).emit(div, item);
           };
-          wiki.putAction(pageDiv, action);
-          return wiki.getPlugin(type).emit(div, item);
         };
-      };
-      dropEvent.preventDefault();
-      console.log([dropEvent, dropEvent.originalEvent]);
-      file = dropEvent.originalEvent.dataTransfer.files[0];
-      console.log([file.type, file]);
-      _ref = file.type.split("/"), majorType = _ref[0], minorType = _ref[1];
-      if (allowedTypes.filter(function(t) {
-        return t === majorType;
-      }).length === 0) {
-        return alert("Uploads of type " + majorType + " not supported for this item");
-      } else {
-        reader = new FileReader();
-        if (majorType === "image") {
-          reader.onload = finishDrop("image", function(loadEvent) {
-            item.url = loadEvent.target.result;
-            return item.caption || (item.caption = "Uploaded image");
-          });
-          return reader.readAsDataURL(file);
-        } else if (majorType === "text") {
-          reader.onload = finishDrop("paragraph", function(loadEvent) {
-            return item.text = loadEvent.target.result;
-          });
-          return reader.readAsText(file);
+        dropEvent.preventDefault();
+        if (dropEvent.originalEvent.dataTransfer != null) {
+          console.log(dropEvent.originalEvent.dataTransfer.types);
+          file = dropEvent.originalEvent.dataTransfer.files[0];
+          if (file != null) {
+            _ref = file.type.split("/"), majorType = _ref[0], minorType = _ref[1];
+            reader = new FileReader();
+            if (majorType === "image") {
+              reader.onload = finishDrop("image", function(loadEvent) {
+                item.url = loadEvent.target.result;
+                return item.caption || (item.caption = "Uploaded image");
+              });
+              return reader.readAsDataURL(file);
+            } else if (majorType === "text") {
+              reader.onload = finishDrop("paragraph", function(loadEvent) {
+                return item.text = loadEvent.target.result;
+              });
+              return reader.readAsText(file);
+            } else {
+              return alert("Expected text or image, got '" + file.type + "'");
+            }
+          } else {
+            return alert("Expected file, got null");
+          }
+        } else {
+          return alert("Expected dataTransfer, got null");
         }
-      }
-    });
+      });
+    }
   };
 }).call(this);
