5cac051393309623c2a30f3a397ed9e2bfbc7168
Ward Cunningham <ward@c2.com>
2011-10-30 14:50:59 -0700

    test in place editing

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -43,3 +43,75 @@ describe "loading a page" do
   end
 
 end
+
+# describe "retrieving favicon" do
+#   before do
+#     `rm -rf #{TEST_DATA_DIR}`
+#     Capybara.current_driver = :selenium
+#   end
+#   it "should return the default image when no other image is present" do
+#     File.exist?(File.join(TEST_DATA_DIR, "status/favicon.png")).should == false
+#     visit("/favicon.png")
+#     body.shound include("favicon.png (PNG Image, 32x32 pixels)") #firefox generated
+#   end
+# end
+
+class Capybara::Node::Element
+  def double_click
+    driver.browser.mouse.double_click(native)
+  end
+
+  TRIGGER_JS = "$(arguments[0]).trigger(arguments[1]);"
+  def trigger(event)
+    driver.browser.execute_script(TRIGGER_JS, native, event)
+  end
+end
+
+describe "edit paragraph in place" do
+  before do
+    `rm -rf #{TEST_DATA_DIR}`
+    Capybara.current_driver = :selenium
+    visit("/")
+  end
+
+  def first_paragraph
+    page.find(".paragraph:first")
+  end
+
+  def double_click_paragraph
+    first_paragraph.double_click
+  end
+
+  def text_area
+    first_paragraph.find("textarea")
+  end
+
+  def replace_and_save(value)
+    text_area.set value
+    text_area.trigger "focusout"
+  end
+
+  def journal
+    page.find(".journal").all(".action")
+  end
+
+
+  it "should turn into a text area, showing wikitext when double-clicking" do
+    double_click_paragraph
+    text_area.value.should include("Welcome to the [[Smallest Federated Wiki]]")
+  end
+
+  it "should save changes to wiki text when unfocused" do
+    double_click_paragraph
+    replace_and_save("The [[quick brown]] fox.")
+    first_paragraph.text.should include("The quick brown fox")
+  end
+
+  it "should record edit in the journal" do
+    j = journal.length
+    double_click_paragraph
+    replace_and_save("The [[quick brown]] fox.")
+    journal.length.should == j+1
+  end
+
+end
