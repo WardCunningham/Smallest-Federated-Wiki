ac9dd046db940e8861d04b398bccae22a369b483
Ward Cunningham <ward@c2.com>
2011-11-08 14:56:45 -0800

    add more info in pioneer citations

--- client/plugins/federatedWiki.coffee
+++ client/plugins/federatedWiki.coffee
@@ -1,7 +1,7 @@
 window.plugins.federatedWiki =
   emit: (div, item) ->
     slug = item.slug or 'welcome-visitors'
-    div.append($("<h3><img src='//#{item.site}/favicon.png' class='remote' width='16px' height='16px'> #{item.title}</h3>"))
+    div.append($("<h3><img src='//#{item.site}/favicon.png' class='remote' width='16px' height='16px'> #{item.title || slug}</h3>"))
     div.append($("<p>#{wiki.resolveLinks(item.text)}</p>"))
     div.append($("<p class='cite'>http://#{item.site}/view/#{slug}</p>"))
     div.find('img').data('slug', slug).data('site', item.site)
--- client/plugins/federatedWiki.js
+++ client/plugins/federatedWiki.js
@@ -3,7 +3,7 @@
     emit: function(div, item) {
       var slug;
       slug = item.slug || 'welcome-visitors';
-      div.append($("<h3><img src='//" + item.site + "/favicon.png' class='remote' width='16px' height='16px'> " + item.title + "</h3>"));
+      div.append($("<h3><img src='//" + item.site + "/favicon.png' class='remote' width='16px' height='16px'> " + (item.title || slug) + "</h3>"));
       div.append($("<p>" + (wiki.resolveLinks(item.text)) + "</p>"));
       div.append($("<p class='cite'>http://" + item.site + "/view/" + slug + "</p>"));
       return div.find('img').data('slug', slug).data('site', item.site);
