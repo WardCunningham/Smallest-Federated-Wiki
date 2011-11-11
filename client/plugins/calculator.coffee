8c2cc88e3a1f5934dc7674c91f45849c70db66d4
Ward Cunningham <ward@c2.com>
2011-11-10 23:43:09 -0800

    port awk expense calculator to wiki plugin, issue #53

---
+++ client/plugins/calculator.coffee
@@ -0,0 +1,25 @@
+window.plugins.calculator =
+  emit: (div, item) ->
+    text = calculate(item.text).join "\n"
+    pre = $('<pre style="font-size: 16px;"/>').text text
+    div.append pre
+  bind: (div, item) ->
+    div.dblclick -> wiki.textEditor div, item
+
+calculate = (text) ->
+  [sums, sum] = [{}, 0]
+  for line in text.split "\n"
+    col = line.split /\s+/
+    col[0] = col[0].replace /^[A-Z]+$/, (x) ->
+      [sums[x], sum] = [sum, 0] unless sums[x]? && x != 'SUM' 
+      sums[x].toFixed 2
+    col[0] = col[0].replace /^\-?[0-9\.]+$/, (x) ->
+      sum = sum + dx = switch col[1]
+        when 'CR', 'DB' then x/-1
+        when '*' then x*col[2]
+        when '/' then x/col[2]
+        else x/1
+      (x/1).toFixed 2
+    sum = 0 if line.match /^\s*$/
+    col.join ' '
+
---
+++ client/plugins/calculator.js
@@ -0,0 +1,55 @@
+(function() {
+  var calculate;
+  window.plugins.calculator = {
+    emit: function(div, item) {
+      var pre, text;
+      text = calculate(item.text).join("\n");
+      pre = $('<pre style="font-size: 16px;"/>').text(text);
+      return div.append(pre);
+    },
+    bind: function(div, item) {
+      return div.dblclick(function() {
+        return wiki.textEditor(div, item);
+      });
+    }
+  };
+  calculate = function(text) {
+    var col, line, sum, sums, _i, _len, _ref, _ref2, _results;
+    _ref = [{}, 0], sums = _ref[0], sum = _ref[1];
+    _ref2 = text.split("\n");
+    _results = [];
+    for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+      line = _ref2[_i];
+      col = line.split(/\s+/);
+      col[0] = col[0].replace(/^[A-Z]+$/, function(x) {
+        var _ref3;
+        if (!((sums[x] != null) && x !== 'SUM')) {
+          _ref3 = [sum, 0], sums[x] = _ref3[0], sum = _ref3[1];
+        }
+        return sums[x].toFixed(2);
+      });
+      col[0] = col[0].replace(/^\-?[0-9\.]+$/, function(x) {
+        var dx;
+        sum = sum + (dx = (function() {
+          switch (col[1]) {
+            case 'CR':
+            case 'DB':
+              return x / -1;
+            case '*':
+              return x * col[2];
+            case '/':
+              return x / col[2];
+            default:
+              return x / 1;
+          }
+        })());
+        return (x / 1).toFixed(2);
+      });
+      if (line.match(/^\s*$/)) {
+        sum = 0;
+      }
+      _results.push(col.join(' '));
+    }
+    return _results;
+  };
+}).call(this);
