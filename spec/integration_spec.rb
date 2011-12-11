5fdcbc33f7253078c52640b03568e31013ccab6b
Stephen Judkins <stephen.judkins@gmail.com>
2011-12-11 14:34:51 -0800

    Fixed integration test race conditions
    * Added wrapper around "visit" that makes sure to block until AJAX requests are complete. Uses jQuery ajax support.
    * Added built-in Capybara "resynchronize" wrapper around "drag_to" helper we've written

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -31,14 +31,6 @@ RSpec.configure do |config|
   config.include Capybara::DSL
 end
 
-AJAX_TIMEOUT_LIMIT = 5
-def wait_for_ajax_to_complete!
-  start = Time.now
-  while page.evaluate_script("window.jQuery.active") != 0 do
-    raise Timeout::Error.new("AJAX request timed out") if Time.now - start > AJAX_TIMEOUT_LIMIT
-  end
-end
-
 describe "loading a page" do
   before do
     `rm -rf #{TestDirs::TEST_DATA_DIR}`
@@ -66,7 +58,6 @@ describe "loading a page" do
   it "should load remote page" do
     remote = "localhost:#{Capybara.server_port}"
     visit("/#{remote}/welcome-visitors")
-    wait_for_ajax_to_complete!
     body.should include("You are welcome to copy this page to any server you own and revise its welcoming message as you see fit.")
   end
 
@@ -83,7 +74,9 @@ class Capybara::Node::Element
   end
 
   def drag_down(number)
-    driver.browser.execute_script "$(arguments[0]).simulateDragSortable({move: arguments[1]});", native, number
+    driver.resynchronize do
+      driver.browser.execute_script "$(arguments[0]).simulateDragSortable({move: arguments[1]});", native, number
+    end
   end
 end
 
@@ -97,6 +90,21 @@ class Capybara::Session
       driver.browser.execute_script File.read(file)
     end
   end
+
+  AJAX_TIMEOUT_LIMIT = 5
+  def wait_for_ajax_to_complete!
+    start = Time.now
+    while evaluate_script("window.jQuery.active") != 0 do
+      raise Timeout::Error.new("AJAX request timed out") if Time.now - start > AJAX_TIMEOUT_LIMIT
+    end
+  end
+
+  def visit_with_wait_for_ajax(*args)
+    visit_without_wait_for_ajax(*args)
+    wait_for_ajax_to_complete!
+  end
+  alias_method :visit_without_wait_for_ajax, :visit
+  alias_method :visit, :visit_with_wait_for_ajax
 end
 
 def first_paragraph
