49b6893be0218cb80ed635b4d5950d39d5b18770
Ward Cunningham <ward@c2.com>
2011-12-27 19:37:41 -0800

    readouts for popular data formats

--- client/plugins/data.js
+++ client/plugins/data.js
@@ -1,15 +1,39 @@
 (function() {
+  var readout, thumbs;
+  var __hasProp = Object.prototype.hasOwnProperty;
   window.plugins.data = {
     emit: function(div, item) {
-      $('<p />').addClass('readout').appendTo(div).text("" + item.data.length + "x" + item.columns.length);
+      $('<p />').addClass('readout').appendTo(div).text(readout(item));
       return $('<p />').html(wiki.resolveLinks(item.text || 'data')).appendTo(div);
     },
     bind: function(div, item) {
       return div.find('p:first').mousemove(function(e) {
-        var column;
-        column = item.columns[Math.floor(item.columns.length * e.offsetX / e.target.offsetWidth)];
-        return $(e.target).siblings("p").last().html(column);
+        var thumb;
+        thumb = thumbs(item)[Math.floor(thumbs(item).length * e.offsetX / e.target.offsetWidth)];
+        return $(e.target).siblings("p").last().html(thumb);
       });
     }
+  };
+  readout = function(item) {
+    if (item.columns != null) {
+      return "" + item.data.length + "x" + item.columns.length;
+    }
+    if ((item.data.nodes != null) && (item.data.links != null)) {
+      return "" + item.data.nodes.length + "/" + item.data.links.length;
+    }
+    return "data";
+  };
+  thumbs = function(item) {
+    var key, _ref, _results;
+    if (item.columns != null) {
+      return item.columns;
+    }
+    _ref = item.data;
+    _results = [];
+    for (key in _ref) {
+      if (!__hasProp.call(_ref, key)) continue;
+      _results.push(key);
+    }
+    return _results;
   };
 }).call(this);
