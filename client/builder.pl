4a77bdd213f7059267ced99a9ab9f6e679a1a313
Sven Dowideit <SvenDowideit@fosiki.com>
2012-04-30 12:59:53 +1000

    modify builder.pl so I can use it on linux

--- client/builder.pl
+++ client/builder.pl
@@ -1,18 +1,34 @@
-my ($old, $new);
+#!/usr/bin/perl
 
+#this script exists because the browserify -w option does not appear to work?
+
+my ($old, $new);
+my $OSXsay = ($^O eq 'darwin');
+
+sub say {
+    my $msg = shift;
+    if ($OSXsay) {
+        `say $msg`;
+    } else {
+        print $msg."\n";
+    }
+}
 sub run {
   $trouble = `$_[0] 2>&1`;
   return unless $trouble;
-  print "\n$_[0]\n$trouble";
-  $trouble =~ / on line \d+/;
-  `say having trouble$&.`;
+  if ($trouble =~ /( on line \d+)/) {
+      say("having trouble $1.");
+      print "\n$_[0]\n$trouble";
+  }
 }
 
 while (sleep 1) {
   $new = `ls -lt *.coffee lib/*.coffee test/*.coffee`;
   next if $old eq $new;
   $old = $new;
-  run 'say client& browserify client.coffee -o client.js';
-  run 'say test& browserify testclient.coffee -o test/testclient.js';
-  `say done.&`;
+  say('client.');
+  run('npm start');
+  say('test.');
+  run('npm test');
+  say('done.');
 }
