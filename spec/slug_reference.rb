709041d04feed9c6e665d7f1caec397131d3a7a9
Harlan T Wood <harlan@thegoldensun.com>
2012-06-06 12:21:00 -0700

    Move slug generation into FedWiki.slug.
    To run: bundle exec rspec spec/slug_reference.rb

--- spec/slug_reference.rb
+++ spec/slug_reference.rb
@@ -1,36 +1,47 @@
-def asSlug (name)
-  name.gsub(/\s/, '-').gsub(/[^A-Za-z0-9-]/, '').downcase()
-end
+# encoding: UTF-8
+require "rspec"
+require_relative "../server/sinatra/slug"
 
 def section (comment)
-  puts "\n\t#{comment}\n"
+  $section = comment
+  $counter = 0
 end
 
 def test (given, expected)
-  actual = asSlug given
-  puts actual == expected ? "OK\t#{given}" : "YIKES\t#{given} => #{actual}, not #{expected} as expected"
+  describe "#{$section}: Test ##{$counter+=1}" do
+    it "should convert the string #{given.inspect} to the slug #{expected.inspect}" do
+      FedWiki.slug(given).should == expected
+    end
+  end
 end
 
 # the following test cases presume to be implementation language agnostic
 # perhaps they should be included from a common file
 
-# 'WORKING'
 section 'case and hyphen insensitive'
 test 'Welcome Visitors', 'welcome-visitors'
 test 'welcome visitors', 'welcome-visitors'
 test 'Welcome-visitors', 'welcome-visitors'
-
+test 'slugs-are-unchanged', 'slugs-are-unchanged'
 
 section 'numbers and punctuation'
 test '2012 Report', '2012-report'
 test 'Ward\'s Wiki', 'wards-wiki'
+test 'O\'malley', 'omalley'
+test 'holy cats !!! you don\'t say', 'holy-cats-you-dont-say'
+test '---holy cats !!! ---------', 'holy-cats'
+test 'Pride & Prejudice', 'pride-prejudice'
 
-# 'PROBLEMATIC'
 section 'white space insenstive'
 test 'Welcome  Visitors', 'welcome-visitors'
 test '  Welcome Visitors', 'welcome-visitors'
 test 'Welcome Visitors  ', 'welcome-visitors'
 
 section 'foreign language'
-test 'Les Misérables', 'les-misérables'
-test 'Les Misérables', 'les-miserables'
+test 'Les Misérables', 'les-misrables'
+
+section 'edge cases'
+test 'a', 'a'
+test 'ßøå∂ƒ', ''
+test '---', ''
+test ' - - - - ', ''
