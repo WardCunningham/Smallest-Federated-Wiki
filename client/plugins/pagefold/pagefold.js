e1f8046cfe18eba09eefb9f8f60346bdcb687bda
Ward Cunningham <ward@c2.com>
2013-02-01 12:54:14 -0800

    semantically significant whitespace changes

--- client/plugins/pagefold/pagefold.js
+++ client/plugins/pagefold/pagefold.js
@@ -6,11 +6,11 @@
     return $item.append("<div style=\"height: 10px; border-top: 2px solid lightgray; margin-top: 24px; text-align: center; position: relative; clear: both;\">\n	<span style=\"position: relative; top: -.8em; background: white; display: inline-block; color: gray; \">\n		&nbsp; " + item.text + " &nbsp;\n	</span>\n</div>");
   };
 
-  bind = function($item, item) {};
-
-  $item.dblclick(function() {
-    return wiki.textEditor($item, item);
-  });
+  bind = function($item, item) {
+    return $item.dblclick(function() {
+      return wiki.textEditor($item, item);
+    });
+  };
 
   window.plugins.pagefold = {
     emit: emit,
